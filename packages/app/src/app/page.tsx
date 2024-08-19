5dc7046dfd21372b1ef8f1e7cad2f702029c76e0
Matt Whitaker <mattwhitaker.name@gmail.com>
2024-08-19 15:23:51 -0700

    housekeeping

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,15 +1,12 @@
 import Hero from "@/component/hero";
-import Shell from "@/component/shell";
 
 export default function Home() {
     return (
-        <Shell>
-            <Hero>
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold mb-5">Welcome back!</h1>
-                    [Cool HUD / Dashboard]
-                </div>
-            </Hero>
-        </Shell>
+        <Hero>
+            <div className="max-w-md">
+                <h1 className="text-5xl font-bold mb-5">Welcome back!</h1>
+                [Cool HUD / Dashboard]
+            </div>
+        </Hero>
     );
 }
---
+++ packages/app/src/pages/README.md
@@ -0,0 +1,5 @@
+# Why Pages Router?
+
+At this time, the Next.js "App Router" does not support client-side dynamic params, but "Pages Router" does. Since the goal
+of this project is to incrementally add more complex features, sticking with Next.js is prudent, but the core of the
+product is intended to be a completely static Single-Page Application--meaning no servers or backends.
