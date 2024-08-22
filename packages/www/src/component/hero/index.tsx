4c75e67cba76a376d43ead0fa2346c1842a037e8
Matt Whitaker <mattwhitaker.name@gmail.com>
2024-08-22 13:09:38 -0700

    try to fix jenkiness

--- packages/www/src/component/hero/index.tsx
+++ packages/www/src/component/hero/index.tsx
@@ -5,7 +5,7 @@ import {PropsWithClass} from "@brewdocs.beer/core";
 export type HeroProps = Partial<PropsWithClass> & PropsWithChildren & { title: string };
 export default function Hero({ children, title, className }: HeroProps) {
     return (
-        <div className={classNames("h-screen hero overflow-y-auto bg-base-200", [className])}>
+        <div className={classNames("w-screen h-screen hero overflow-y-auto bg-base-200", [className])}>
             <div className="hero-content flex-col text-center">
                 <h1 className="divider text-3xl">{title}</h1>
                 {children}
