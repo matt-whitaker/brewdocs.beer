491479ed00e28cf8f53f9023a935c7dd508d1c8c
Matt Whitaker <mattwhitaker.name@gmail.com>
2024-08-27 15:05:22 -0700

    z fix

--- packages/app/src/component/button-checklist/item.tsx
+++ packages/app/src/component/button-checklist/item.tsx
@@ -12,7 +12,7 @@ export default function ButtonChecklistItem({ name, onToggle, onRemove, checked,
     return (
         <li className="w-full overflow-hidden max-lg:[&>label]:odd:btn-ghost relative lg:[&:hover>div]:flex">
             <div onClick={onRemove} className="absolute lg:right-3 right-2 items-center lg:hidden flex h-full hover:cursor-pointer">
-                <Trash className="w-4 stroke-red-800" />
+                <Trash className="w-4 stroke-red-800 z-50" />
             </div>
             <label htmlFor={name} key={name} className="font-normal justify-start hover:cursor-pointer btn lg:btn-ghost btn-sm mb-0.5 text-lg flex items-center">
                 <input id={name} value={name} onChange={onToggle} type="checkbox" checked={checked} className="checkbox checkbox-xs mr-3" />
