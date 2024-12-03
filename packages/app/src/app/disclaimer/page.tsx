9d1ea9e2ad490e29ed876face3a8080efd80472a
Matt Whitaker <mattwhitaker.name@gmail.com>
2024-12-02 18:27:21 -0800

    orphaned disclaimer page

---
+++ packages/app/src/app/disclaimer/page.tsx
@@ -0,0 +1,11 @@
+
+export default function DisclaimerPage() {
+    return (
+        <div className="max-w-md p-5">
+            <h1 className="font-bold mb-2">Disclaimer</h1>
+            <p>
+                BrewDocs is currently in an early prototyping phase, and thus is not considered stable.
+            </p>
+        </div>
+    );
+}
