f273e6e044eff089f09aa963a0c4a3e0ecb1f1f8
Matt Whitaker <matt-whitaker@users.noreply.github.com>
2026-07-09 00:20:49 -0700

    Migrate www from Next to Astro (#12)
    * gitignore update
    
    * astro setup
    
    * tailwind
    
    * claude migrate
    
    * fix style discrepencies
    
    * stash
    
    * repoint workflow

---
+++ packages/www2/src/data/env.ts
@@ -0,0 +1,2 @@
+export const APP_URL: string = import.meta.env.PUBLIC_APP_URL;
+export const GITHUB_URL: string = import.meta.env.PUBLIC_GITHUB_URL;
---
+++ packages/www2/src/data/nav.ts
@@ -0,0 +1,8 @@
+import {APP_URL} from "@/data/env";
+
+const nav: [string, string, boolean?][] = [
+    ["About", "/about"],
+    ["Go to app", APP_URL, true]
+]
+
+export default nav;
