483e950fcdd46bb6493307fee2a892f58ecc9b1f
Matt Whitaker <mattwhitaker.name@gmail.com>
2026-07-09 16:40:03 -0700

    step forward

--- packages/app/src/component/collapse/index.tsx
+++ packages/app/src/component/collapse/index.tsx
@@ -6,7 +6,7 @@ export type CollapseProps = { title: string; openInitial?: boolean; toggle?: (op
 export default function Collapse({ children, title, className, openInitial, toggle }: CollapseProps) {
     const onChange = useCallback(({ target }: { target: HTMLInputElement }) => toggle?.(target.checked), [toggle]);
     return (
-        <div tabIndex={0} className={classNames("collapse max-lg:collapse-arrow min-h-2", [className])}>
+        <div className={classNames("collapse max-lg:collapse-arrow min-h-2", [className])}>
             <input className="min-h-2 peer" type="checkbox" id={`collapse-${title}`} defaultChecked={!!openInitial} onChange={onChange} />
             <label htmlFor={`collapse-${title}`} className="min-h-2 collapse-title text-xl font-medium px-0 py-1 after:-mr-2 after:-mt-3">{title}</label>
             <div className="collapse-content px-0 pb-2 peer-checked:pb-0">
