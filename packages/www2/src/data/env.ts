1e02b21a42cb56e8653b36c1bb64abcf95a466a6
Matt Whitaker <mattwhitaker.name@gmail.com>
2026-07-09 00:48:36 -0700

    missing vars

--- packages/www2/src/data/env.ts
+++ packages/www2/src/data/env.ts
@@ -1,2 +1,2 @@
-export const APP_URL: string = import.meta.env.PUBLIC_APP_URL;
-export const GITHUB_URL: string = import.meta.env.PUBLIC_GITHUB_URL;
+export const APP_URL: string = import.meta.env.PUBLIC_APP_URL || "https://app.brewdocs.beer";
+export const GITHUB_URL: string = import.meta.env.PUBLIC_GITHUB_URL || "https://github.com/matt-whitaker/brewdocs.beer";
