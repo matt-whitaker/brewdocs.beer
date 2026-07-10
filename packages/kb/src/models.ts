aeec0eecb9fc910b122fdb4cd757127876bb2ac7
Matt Whitaker <mattwhitaker.name@gmail.com>
2026-07-10 13:41:48 -0700

    kb model cleanup

--- packages/kb/src/models.ts
+++ packages/kb/src/models.ts
@@ -1,7 +1,75 @@
 import {Entity} from "@brewdocs.beer/core";
 
-export interface KbRecipe extends Entity {
+export interface KbScalar {
+    value: string;
+    unit: string;
+}
 
+export interface KbRecipe extends Entity {
+    name: string;
+    brewer: string;
+    description: string;
+    type: string;
+    batchSize: KbScalar;
+    boilTime: KbScalar;
+    efficiency: KbScalar;
+
+    grains: {
+        name: string;
+        weight: KbScalar;
+    }[];
+
+    mash: {
+        name: string;
+        temp: KbScalar;
+        time: KbScalar;
+        grains: string;
+    }[];
+
+    boil: {
+        name: string;
+        time: KbScalar;
+        hops: string;
+    }[];
+
+    hops: {
+        name: string;
+        weight: KbScalar;
+        alpha: KbScalar;
+        boil: KbScalar;
+        phase: string;
+    }[];
+
+    yeast: {
+        name: string;
+        avg_attn: KbScalar;
+        temp: KbScalar;
+        starter: boolean;
+    }[];
+
+    additives: {
+        name: string;
+        boil: KbScalar;
+    }[];
+
+    equipment: {
+        name: string;
+        use: string[];
+        count?: number;
+    }[];
+
+    checklist: {
+        name: string;
+        uses: string[];
+    }[];
+
+    targets: {
+        og: KbScalar;
+        fg: KbScalar;
+        abv: KbScalar;
+        ibu: string;
+        srm: string;
+    };
 }
 
 export interface KbGrain extends Entity {
