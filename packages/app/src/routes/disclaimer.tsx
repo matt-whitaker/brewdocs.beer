d58c28c90f5aa9f8fb3f8ab20f2d73f836d45038
Matt Whitaker <mattwhitaker.name@gmail.com>
2026-07-12 01:51:56 -0700

    clearer purge

--- packages/app/src/routes/disclaimer.tsx
+++ packages/app/src/routes/disclaimer.tsx
@@ -1,4 +1,5 @@
 import {createFileRoute} from "@tanstack/react-router";
+import {Plus} from "@/component/svg";
 
 export const Route = createFileRoute("/disclaimer")({
     component: DisclaimerPage
@@ -10,7 +11,8 @@ function DisclaimerPage() {
             <h1 className="font-bold mb-2 text-2xl">Disclaimer</h1>
             <p>BrewDocs is currently in an early prototyping phase, and thus is not considered stable. It is mostly deployed for demo purposes.</p>
             <div className="divider divider-secondary"></div>
-            <p>If you run into issues with, you can add <code className="bg-gray-200 whitespace-nowrap">?purge=true</code> to the URL to clear your db and cache.</p>
+            <p>If you run into issues with it, you can use the button below to perform a wipe and start fresh.</p>
+            <p><a className="btn btn-primary btn-sm mt-1" href="/?purge=true">Purge Data</a></p>
         </div>
     );
 }
