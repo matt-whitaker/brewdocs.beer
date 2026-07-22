b2db1b6202cbd7807246ec4ab767193546e5643f
Matt Whitaker <mattwhitaker.name@gmail.com>
2026-07-22 13:41:41 -0700

    fix

--- packages/app/src/screen/recipe-edit-schedule/index.tsx
+++ packages/app/src/screen/recipe-edit-schedule/index.tsx
@@ -5,6 +5,7 @@ import RecipeEditBoil from "@/screen/recipe-edit-schedule/boil";
 import RecipeEditEquipment from "@/screen/recipe-edit-schedule/equipment";
 import RecipeEditMash from "@/screen/recipe-edit-schedule/mash";
 import {saveRecipe, useRecipe} from "@/state/recipes";
+import Screen from "@/component/screen";
 
 export type RecipeEditScheduleProps = { recipeId: string };
 export default function RecipeEditSchedule({ recipeId }: RecipeEditScheduleProps) {
@@ -13,31 +14,27 @@ export default function RecipeEditSchedule({ recipeId }: RecipeEditScheduleProps
     const [data, update, updateScalar, add, remove] = useJsonEdit<Recipe>(recipe, onChange);
 
     return (
-        <div className="pt-2 grid grid-cols-1 lg:grid-cols-2 gap-x-4">
-            <div>
-                <RecipeEditMash
-                    mash={data.mash}
-                    add={add}
-                    remove={remove}
-                    update={update}
-                    updateScalar={updateScalar}
-                />
-                <RecipeEditBoil
-                    boil={data.boil}
-                    add={add}
-                    remove={remove}
-                    update={update}
-                    updateScalar={updateScalar}
-                />
-            </div>
-            <div>
-                <RecipeEditEquipment
-                    equipment={data.equipment}
-                    add={add}
-                    remove={remove}
-                    update={update}
-                />
-            </div>
-        </div>
+        <Screen>
+            <RecipeEditMash
+                mash={data.mash}
+                add={add}
+                remove={remove}
+                update={update}
+                updateScalar={updateScalar}
+            />
+            <RecipeEditBoil
+                boil={data.boil}
+                add={add}
+                remove={remove}
+                update={update}
+                updateScalar={updateScalar}
+            />
+            <RecipeEditEquipment
+                equipment={data.equipment}
+                add={add}
+                remove={remove}
+                update={update}
+            />
+        </Screen>
     );
 }
