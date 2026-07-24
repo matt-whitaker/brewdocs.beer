4cba295c84a976c5364da1df386e4b5fce0ee6d4
Matt Whitaker <mattwhitaker.name@gmail.com>
2026-07-23 20:58:38 -0700

    tweak

--- packages/app/src/screen/recipe-overview/index.tsx
+++ packages/app/src/screen/recipe-overview/index.tsx
@@ -1,4 +1,4 @@
-import {ScreenP} from "@brewdocs.beer/design";
+import {ScreenP, ScreenH2} from "@brewdocs.beer/design";
 import Organics from "@/component/organics";
 import Screen from "@/component/screen";
 import {RecipeSource, useRecipeResource} from "@/state/recipeResource";
@@ -10,6 +10,7 @@ export default function RecipeOverview({ recipeId, source }: RecipeOverviewProps
     return (
         <Screen>
             <div className="lg:max-w-[80%] lg:pb-4 pb-2 pt-2">
+                <ScreenH2>{recipe.name}</ScreenH2>
                 <ScreenP>By {`${recipe.brewer}`}</ScreenP>
                 <ScreenP className="pt-2">ABV {recipe.targets.abv.value}% | IBUs {recipe.targets.ibu} | O.G. {recipe.targets.og.value} | F.G. {recipe.targets.fg.value}</ScreenP>
                 <ScreenP className="pt-4">{`${recipe.description}`}</ScreenP>
